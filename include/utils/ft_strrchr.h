804496a24054f35fa0e2ede14a4801f4dc5136d7
Alexander derck Horlings <horlingsalexander@gmail.com>
2023-04-23 22:39:05 +0200

    normed once more

--- include/utils/ft_strrchr.h
+++ include/utils/ft_strrchr.h
@@ -6,12 +6,12 @@
 /*   By: ahorling <ahorling@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/23 21:39:32 by ahorling      #+#    #+#                 */
-/*   Updated: 2023/04/23 21:40:09 by ahorling      ########   odam.nl         */
+/*   Updated: 2023/04/23 22:38:28 by ahorling      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_STRRCHR
-# define FT_STRRCHR
+#ifndef FT_STRRCHR_H
+# define FT_STRRCHR_H
 
 char	*ft_strrchr(const char *str, int c);
 
